bd7a2bc28a71b07762cf9c4da92330bfd1f5fa4c
eunsookim1 <92eunsoo@gmail.com>
2023-03-24 23:24:50 +0900

    lotide challenge

---
+++ head.js
@@ -0,0 +1,37 @@
+// output: first element of the array
+// output: undefined if the array is empty
+
+let head = function (array) {
+  let num = array[0];
+  if(array.length > 0) {
+  console.log(num);
+  return num;
+} else {
+  console.log(undefined);
+  return undefined; 
+}
+};
+
+const assertEqual = function(actual, expected) {
+
+  let assertion = '';
+  
+  if (actual === expected) {
+    assertion = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+     
+  } else if (actual !== expected) {
+    assertion = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    
+  }
+  console.log(assertion);
+  return assertion;
+  
+  
+};
+
+
+// TEST CODE:
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([]), 5);
