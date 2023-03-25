f5e92debfc63c0f045885b7ec86a3c472962febe
eunsookim1 <92eunsoo@gmail.com>
2023-03-25 16:28:36 +0900

    reversing a string and using process.argv

---
+++ reverse.js
@@ -0,0 +1,23 @@
+/**
+ * Write a program that takes any number of command line arguments, all strings
+ * and reverses them before outputting them one at a time to the console.
+ 
+*/
+
+const reverse = function(array) {
+  let word = "";
+  for (let i = array.length - 1; i >= 0; i--) {
+    word += array[i];
+  }
+  console.log(word);
+  return word;
+};
+
+
+
+let args = process.argv.slice(2);
+for (let i = 0; i < args.length; i++) {
+  reverse(args[i]);
+}
+
+
