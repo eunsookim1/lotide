8da345631e3a55f7d36933352149dc9b88204da3
eunsookim1 <92eunsoo@gmail.com>
2023-03-27 00:57:26 +0900

    assertion using eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,33 @@
+let eqArrays = function (array1, array2) {
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+
+const assertArraysEqual = function(array1, array2) {
+  
+  let result = eqArrays(array1, array2);
+  if (result) {
+    
+    // result === true, some values will considered true. result is a boolean, considered true.
+
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+     
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    
+  }
+};
+
+
+assertArraysEqual([1, 2, 4], [1, 2, 3]);
+
+
+
