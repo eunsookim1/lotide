a01e65f6b9b373a6e5ee9d080b04823b3ba1c124
eunsookim1 <92eunsoo@gmail.com>
2023-03-27 15:12:19 +0900

    flattening an array

---
+++ flatten.js
@@ -0,0 +1,46 @@
+let eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  
+  let result = eqArrays(array1, array2);
+  if (result) {
+    
+    // result === true, some values will considered true. result is a boolean, considered true.
+
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+     
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const flatten = (array) => {
+  const result = [];
+  array.forEach((elem) => {
+    if (Array.isArray(elem)) {
+      elem.forEach((nestedElem) => {
+        result.push(nestedElem);
+      });
+    } else {
+      result.push(elem);
+    }
+  });
+
+  return result;
+};
+
+
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
