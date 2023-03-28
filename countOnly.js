65cf5b1d63aa35b9ead3e6f1d1f7aaf275fd7f65
eunsookim1 <92eunsoo@gmail.com>
2023-03-28 17:15:43 +0900

    working with object

---
+++ countOnly.js
@@ -0,0 +1,73 @@
+// Helping an event planner who is planning a conference.
+// They have a list of all the attendees, but they need to count the # of times certain names appear on the list. Want to make sure the VIP guests are attended properly.
+
+
+// Items in our case will be limited to strings.
+
+const assertEqual = function(actual, expected) {
+
+  let assertion = '';
+  
+  if (actual === expected) {
+    assertion = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+     
+  } else if (actual !== expected) {
+    assertion = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    
+  }
+  console.log(assertion);
+  return assertion;
+  
+};
+
+let eqArrays = function(array1, array2) {
+  if (array1 === array2) {
+    return true;
+  }
+};
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    // inside the loop,
+    // increment the counter for each item:
+    // set a property with tht string key to:
+    // the value that was already there (or zero if nothing there) with 1 added to it.
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+  
+    }
+    console.log(item);
+  }
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
