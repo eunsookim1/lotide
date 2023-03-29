4fe6a5f476c14c7a27e9cd0c78a2e2cfeba2ecd2
eunsookim1 <92eunsoo@gmail.com>
2023-03-29 12:37:26 +0900

    fix errors

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -14,32 +14,6 @@ const assertEqual = function(actual, expected) {
   
 };
 
-let eqArrays = function(array1, array2) {
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  
-  let result = eqArrays(array1, array2);
-  if (result) {
-    
-    // result === true, some values will considered true. result is a boolean, considered true.
-
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-     
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    
-  }
-};
-
-
 // const obj = {k1:v1,k2:v2}
 
 // for(let x in obj){
