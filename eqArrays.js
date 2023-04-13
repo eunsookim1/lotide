a6ceccb0b486f84f4643bffa8157e416631c16e6
eunsookim1 <92eunsoo@gmail.com>
2023-04-14 02:33:29 +0900

    use module.exports to export the function. use test codes in a separate file

--- eqArrays.js
+++ eqArrays.js
@@ -1,47 +1,21 @@
-const assertEqual = function(actual, expected) {
-
-  let assertion = '';
-  
-  if (actual === expected) {
-    assertion = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-     
-  } else if (actual !== expected) {
-    assertion = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-    
-  }
-  console.log(assertion);
-  return assertion;
-  
-  
-};
-
 // Implement a function eqArrays
 // Input: takes two arrays
 // Output: returns true or false, based on perfect match.
 
 let eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
+    console.log(false);
     return false;
   }
   
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
+      console.log(false);
       return false;
     }
   }
+  console.log(true);
   return true;
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp"); // 🛑🛑🛑 Assertion Failed:
-assertEqual(1, 1); // ✅✅✅ Assertion Passed: 1 === 1
-
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-
-
-assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
