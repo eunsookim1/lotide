d7183897102b6efdfbccfec946699c9c58c1a54f
eunsookim1 <92eunsoo@gmail.com>
2023-04-14 14:43:06 +0900

    module.exports in an object

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,16 +1,4 @@
-let eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}; // for comparing arrays in objects
+const eqArrays = require('./eqArrays');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
