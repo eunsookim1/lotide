6d8d98e39b3397cfd0e58fe8b000301d130c5559
eunsookim1 <92eunsoo@gmail.com>
2023-04-16 01:39:50 +0900

    counts fr a specific subset of the items

--- countOnly.js
+++ countOnly.js
@@ -1,40 +1,14 @@
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-
     if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
-        // console.log("B", results[item]);
-      } else {
-        results[item] = 1;
       }
+      results[item] = 1;
     }
-    // console.log("A", results[item]);
   }
   return results;
 };
 
 module.exports = countOnly;
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-module.exports = firstNames;
-
-// TEST CODE:
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// assertEqual(result1["Jason"], 1); // 1 === 1
-// assertEqual(result1["Karima"], undefined); // no Karima thus undefined from obj
-// assertEqual(result1["Fang"], 2); // Fang appeared twice thus 2 === 2
-// assertEqual(result1["Agouhanna"], undefined); // not counting Agouhanna thus undefined === undefined
