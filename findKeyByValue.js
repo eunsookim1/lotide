c50dfdb987874df0558fb5da9b5d4b91ec84d2b4
eunsookim1 <92eunsoo@gmail.com>
2023-04-16 01:40:58 +0900

    takes an object and value and returns the first key that corresponds to the value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,15 +7,3 @@ const findKeyByValue = function(shows, showValue) {
 };
 
 module.exports = findKeyByValue;
-
-// Test code:
-// const bestTVShowsByGenre = {
-//   // eslint-disable-next-line camelcase
-//   sci_fi: "The Expanse",
-//   comedy: "Brooklyn Nine-Nine",
-//   drama:  "The Wire"
-// };
-
-// const assertEqual = require('./assertEqual');
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
