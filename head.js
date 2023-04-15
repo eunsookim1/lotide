89b57fa86941a8fbcc6532401b65d41fc93fdf56
eunsookim1 <92eunsoo@gmail.com>
2023-04-16 01:41:58 +0900

    returns the first value from the array

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
 let head = function(array) {
   let num = array[0];
   if (array.length > 0) {
-    console.log(num);
     return num;
   }
 };
 
 module.exports = head;
-
-
-// output: first element of the array
-// output: undefined if the array is empty
-
-// const {assertEqual} = require('./assertEqual');
-// const importedFunctions = require('./assertEqual');
-// const assertEqual1 = importedFunctions.assertEqual;
