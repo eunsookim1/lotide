3497da72053417a0d0038eb7d800a50d53680355
eunsookim1 <92eunsoo@gmail.com>
2023-04-16 01:40:30 +0900

    takes an object and callback to return the first key meeting specific criteria in callback

--- findKey.js
+++ findKey.js
@@ -1,30 +1,9 @@
-const findKey = function(obj, callback) { /*generic names*/
+const findKey = function(obj, callback) {
   for (let key of Object.keys(obj)) {
     if (callback(obj[key])) {
-      console.log(key);
       return key;
-
     }
   }
 };
 
 module.exports = findKey;
-
-// findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2); // => "noma"
-
-// // Test Code:
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "noma");
