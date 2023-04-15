9d419e337356f2c5217e7047fbcfcfee19ab00f3
eunsookim1 <92eunsoo@gmail.com>
2023-04-16 01:45:42 +0900

    cleared unnecessary comments. returning array sliced based on callback's criteria

--- takeUntil.js
+++ takeUntil.js
@@ -7,25 +7,6 @@ const takeUntil = function(array, callback) {
       return result;
     }
   }
-  return result;
 };
 
 module.exports = takeUntil;
-
-// TEST CODE:
-// const eqArrays = require('./eqArrays');
-// const assertArraysEqual = require('./assertArraysEqual);
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5]; // ==> [ 1, 2, 5, 7, 2 ]
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-
-// console.log('---'); // ==> --
-
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"]; // ==> [ 'I\'ve', 'been', 'to', 'Hollywood' ]
-// const results2 = takeUntil(data2, x => x === ',');
-// console.log(results2);
-
-
-// assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
-// assertArraysEqual(takeUntil(data2,x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-
